705c9900cf0f2d5ee2138821aa5662281e0ad583
Crowdbotics <team@crowdbotics.com>
2021-10-15 22:05:19 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "withered_shadow_31459",
+  url: "https://withered_shadow_31459.botics.co",
+  api: "https://withered_shadow_31459.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
